2455c61b08639465e454a692a1b4ae21b0460275
pauloskaterock <pauloengenharia5@yahoo.com>
2026-09-21 09:17:47 -0300

    prompts, automations

---
+++ Boring_Stuffs_Python/phoneAndEmail.py
@@ -0,0 +1,29 @@
+#! python3
+# phoneAndEmail.py – Encontra números de telefone e endereços de email no clipboard.
+
+
+#----------------Passo 1: Criar uma regex para números de telefone------------------
+import pyperclip, re
+
+# phoneRegex = re.compile(r'''(
+#     (\d{3}|\(\d{3}\))?                # código de área
+#     (\s|-|\.)?                        # separador
+#     (\d{3})                           # primeiros 3 dígitos
+#     (\s|-|\.)                         # separador
+#     (\d{4})                           # últimos 4 dígitos
+#     (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extensão
+#     )''', re.VERBOSE)
+
+
+# TODO: Encontra correspondências no texto do clipboard.
+
+# TODO: Copia os resultados para o clipboard.
+
+# TODO: Cria a regex para email.
+
+# TODO: Encontra correspondências no texto do clipboard.
+
+# TODO: Copia os resultados para o clipboard
+
+
+#------Passo 2: Criar uma regex para endereços de em---------------------------
